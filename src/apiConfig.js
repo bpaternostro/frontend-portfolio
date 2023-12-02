f9b93e31813e8d8305c1aae0670cb0cb93ae49cc
Bruno <bruno.paternostro@gmail.com>
2023-12-02 18:02:02 -0300

    fix: https

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,5 @@
 const ENV = "prod"
-const API_BASE_URL = ENV == "prod" ? 'http://bpaternostro.site/api' : "http://localhost:8000/api";
+const API_BASE_URL = ENV == "prod" ? 'https://bpaternostro.site/api' : "http://localhost:8000/api";
 
 export const API_ENDPOINTS = {
   getPerson: `${API_BASE_URL}/person/1`,
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import GlobalContextProvider from './context/GlobalContextProvider.jsx'
 import ScrollToTop from "./components/ScrollToTop";
+import dotenv from 'dotenv';
 
+// Load environment variables
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <GlobalContextProvider>
