583ada3469bcbd862af2a0e99ee0014348c0423b
Bruno <bruno.paternostro@gmail.com>
2023-12-05 20:37:24 -0300

    fix: apiconfig!

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,5 @@
 const ENV = "prod"
-const API_BASE_URL = ENV == "prod" ? '' : "http://localhost:8000";
+const API_BASE_URL = ENV == "prod" ? 'https://bpaternostro.site' : "http://localhost:8000";
 export const API_ENDPOINTS = {
   getPerson: `${API_BASE_URL}/api/person/1`,
   postReaction: `${API_BASE_URL}/api/post`,
